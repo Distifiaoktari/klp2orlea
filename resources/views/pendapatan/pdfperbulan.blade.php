ee9fd07007c178a3cca6e9d4d8c3c45cabbaa7c7
kemalhiero <kemrohuero@gmail.com>
2022-01-23 20:59:43 +0700

    update2 pendapatan

--- resources/views/pendapatan/pdfperbulan.blade.php
+++ resources/views/pendapatan/pdfperbulan.blade.php
@@ -3,46 +3,47 @@
 
     <head>
         <title>laporan perbulan</title>
-        <link rel="stylesheet" type="text/css"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <style>
+            table, th, td {
+                    border: 1px solid black;
+                    border-collapse: collapse;
+                    padding: 10px;
+                    margin-left:auto;margin-right:auto;
+                    }
+        </style>
     </head>
 
     <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="table">
-                        <br>
-                        <center><h2>Laporan Pendapatan Barbershop Per Bulan</h2></center>
-                        <br>
-                        <table class="table table-bordered border-dark">
-                            <thead>
-                            <tr>
-                                <th scope="col">No</th>
-                                <th scope="col">Bulan</th>
-                                <th scope="col">Banyak Transaksi</th>
-                                <th scope="col">Pendapatan</th>
-                                <th scope="col">Keterangan</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <?php $no=1; ?>
-                            @foreach($tanggal as $bln)
-                            <tr>
-                                <td>{{ $no++ }}</td>
-                                <td>{{$bln->new_date}}</td>
-                                <td>{{$bln->{'count(id)'};}}</td>
-                                <td>tes wei</td>
-                                <td>-</td>
-                            </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                        <?php print_r($tanggal)?>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <center>
+          <h2>Laporan Pendapatan Barbershop Per Bulan</h2>
+        
+        <table>
+            <thead>
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Bulan</th>
+                <th scope="col">Banyak Transaksi</th>
+                <th scope="col">Pendapatan</th>
+                <th scope="col">Keterangan</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php $no=1; ?>
+            @foreach($tanggal as $bln)
+            <tr>
+                <td>{{ $no++ }}</td>
+                <td>{{$bln->new_date}}</td>
+                <td>{{$bln->{'count(id)'};}}</td>
+                <td>-</td>
+                <td>-</td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>  
+        </center>
+        
+                    
     </body>
 
 </html>
