a1bf75da07b4c86e17e1c3ca27664535d6c75dcc
FathiaR <fathiarahma.101@gmail.com>
2022-01-23 22:37:19 +0700

    perbaikan laporan transaksi

--- app/Models/LaporanTransaksiModel.php
+++ app/Models/LaporanTransaksiModel.php
@@ -10,12 +10,4 @@ class LaporanTransaksiModel extends Model
 {
     use HasFactory;
     protected $table = 'pemesanan';
-    public function allData()
-    {
-        return DB::table('pemesanan')
-        ->select('pemesanan.id', 'pelanggan.nama_pelanggan','barberman.nama_barberman', 'pemesanan.tanggal_order', 'pemesanan.status_bayar')
-        ->leftJoin('pelanggan', 'pelanggan.id', '=' , 'pemesanan.pelanggan_id')
-        ->leftJoin('barberman', 'barberman.id', '=' , 'pemesanan.barberman_id')
-        ->get();
-    }
 }
