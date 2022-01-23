d476b008d9f67483d26945c650501ad15eeb520e
Distifiaoktari <73465629+Distifiaoktari@users.noreply.github.com>
2022-01-23 20:03:09 +0700

    Update v_home.blade.php

--- resources/views/v_home.blade.php
+++ resources/views/v_home.blade.php
@@ -3,6 +3,12 @@
 @section('title','Home')
 
 @section('content')
-    <h1>Ini halaman home </h1>
+    <h1>BARBERSHOP KAMI</h1>
+    <h4>Kami barbershop adalah tempat cukur rambut di Indonesia. 
+        Sejak didirikan pada tahun 2021,Barbershop ini terus berkembang. Pada tahun 2022 kami berencana untuk memperluas layanan kami</h4>
+    <h1>LAYANAN : </h1>
+    <h4> Potong Rambut </h4>
+    <h4> Coloring Rambut </h4>
+    <h4> Cuci Rambut </h4>
 
 @endsection
